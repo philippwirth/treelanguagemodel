f6438a5257bec1d044b8627061f9a94a26a3fd0e
Philipp Wirth <pwirth@student.ethz.ch>
2019-03-05 14:40:14 +0100

    add run treelang small shell

---
+++ run_treelang_small.sh
@@ -0,0 +1,1 @@
+python main.py --data "data/treelang_small/" --model "LSTM" --emsize 4 --nhid 2 --nlayers 1 --epochs 200 --batch_size 20 --bptt 8 --dropout 0.1 --dropouth 0.1 --dropouti 0.1 --dropoute 0.1 --wdrop=0.1 --seed=500 --log-interval 10  

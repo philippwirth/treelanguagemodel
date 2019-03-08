2c5a874eca5862399884e5c06e5b1b2a0f237fb3
Philipp Wirth <pwirth@student.ethz.ch>
2019-03-08 09:48:48 +0100

    add support for RNNs

--- merity/model.py
+++ merity/model.py
@@ -15,7 +15,11 @@ class RNNModel(nn.Module):
         self.hdrop = nn.Dropout(dropouth)
         self.drop = nn.Dropout(dropout)
         self.encoder = nn.Embedding(ntoken, ninp)
-        assert rnn_type in ['LSTM', 'QRNN', 'GRU'], 'RNN type is not supported'
+        assert rnn_type in ['LSTM', 'QRNN', 'GRU', 'RNN'], 'RNN type is not supported'
+        if rnn_type == 'RNN':
+            self.rnns = [torch.nn.RNN(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), 1, dropout=0) for l in range(nlayers)]
+            if wdrop:
+                self.rnns = [WeightDrop(rnn, ['weigth_hh_l0'], dropout=wdrop) for rnn in self.rnns]
         if rnn_type == 'LSTM':
             self.rnns = [torch.nn.LSTM(ninp if l == 0 else nhid, nhid if l != nlayers - 1 else (ninp if tie_weights else nhid), 1, dropout=0) for l in range(nlayers)]
             if wdrop:

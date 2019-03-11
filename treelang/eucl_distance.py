1a4f45fb200b47ea11e0ea39e2f40db03fac890f
Philipp Wirth <pwirth@student.ethz.ch>
2019-03-11 15:34:09 +0100

    jap

--- treelang/eucl_distance.py
+++ treelang/eucl_distance.py
@@ -31,6 +31,9 @@ def eucl_entailcone_dist(hidden, next_hidden):
 	inpt2[inpt2 == 0] = 1e-5
 	inpt = torch.div(inpt1, inpt2)
 
+	print(hidden)
+	print(next_hidden)
+	print(' abc ')
 	print(pw_dist)
 	print(hidden_len)
 	print(next_hidden_len)

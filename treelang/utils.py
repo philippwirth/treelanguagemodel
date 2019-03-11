0c260426906184bf6ac5b6682f556e12bf61c41c
Philipp Wirth <pwirth@student.ethz.ch>
2019-03-11 20:54:10 +0100

    seq_len - 1

--- treelang/utils.py
+++ treelang/utils.py
@@ -40,7 +40,7 @@ def batchify_treelang(data, bsz, args):
 
 
 def get_batch(source, i, args, seq_len=None, evaluation=False):
-    seq_len = min(seq_len if seq_len else args.bptt, len(source) - 1 - i)
+    seq_len = min(seq_len-1 if seq_len else args.bptt, len(source) - 1 - i)
     data = source[i:i+seq_len]
     target = source[i+1:i+1+seq_len].view(-1)
     return data, target

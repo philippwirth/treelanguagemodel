0c7bf46c924a5e90ddda1e5a271f5bf07f2052d2
Philipp Wirth <pwirth@student.ethz.ch>
2019-03-15 13:39:25 +0100

    afg

--- main_treelang.py
+++ main_treelang.py
@@ -88,8 +88,8 @@ if torch.cuda.is_available():
 
 
 args.dumpat = 0
-temps = [20, 40, 60, 80]
-#seeds = [1468]
+temps = [10, 20, 30, 40, 50]
+seeds = [1020, 1468]
 best =[100, 0, 0]
 
 for temp in temps:

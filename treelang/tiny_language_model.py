32d11e38ced36cb609ddec117b3ebc8d31a0e29d
Philipp Wirth <pwirth@student.ethz.ch>
2019-03-21 10:06:29 +0100

    fix all indents

--- treelang/tiny_language_model.py
+++ treelang/tiny_language_model.py
@@ -37,9 +37,9 @@ class TinyLanguageModel():
 		# initialize model, criterion and optimizer
 		self.model, self.criterion = self._build_model()
 	    if self.args.optimizer == 'sgd':
-	        self.optimizer = torch.optim.SGD(self.params, lr=self.args.lr, weight_decay=self.args.wdecay)
-	    if self.args.optimizer == 'adam':
-	        self.optimizer = torch.optim.Adam(self.params, lr=self.args.lr, weight_decay=self.args.wdecay)
+	    	self.optimizer = torch.optim.SGD(self.params, lr=self.args.lr, weight_decay=self.args.wdecay)
+		if self.args.optimizer == 'adam':
+			self.optimizer = torch.optim.Adam(self.params, lr=self.args.lr, weight_decay=self.args.wdecay)
 
 		# collect all parameters and finish up initialization
 		self.params = list(self.model.parameters()) + list(self.criterion.parameters())
@@ -54,7 +54,7 @@ class TinyLanguageModel():
 
 	def _model_save(self, fn):
 		with open(fn, 'wb') as f:
-        	torch.save([self.model, self.criterion, self.optimizer], f)
+			torch.save([self.model, self.criterion, self.optimizer], f)
 
 	def _load_data(self):
 
@@ -65,12 +65,12 @@ class TinyLanguageModel():
 		# hash
 		fn = 'corpus.{}.data'.format(hashlib.md5(self.args.data.encode()).hexdigest())
 		if os.path.exists(fn):
-		    print('Loading cached dataset...')
-		    corpus = torch.load(fn)
+			print('Loading cached dataset...')
+			corpus = torch.load(fn)
 		else:
-		    print('Producing dataset...')
-		    corpus = data.Corpus(args.data)
-		    torch.save(corpus, fn)
+			print('Producing dataset...')
+			corpus = data.Corpus(args.data)
+			torch.save(corpus, fn)
 
 		# need to batchify differently for the treelang data
 		train_data = batchify_treelang(corpus.train, self.batch_size, self.args)
@@ -133,48 +133,48 @@ class TinyLanguageModel():
 	def _evaluate(self, data_source, batch_size=1, dump_vars=None):
 
 		# Turn on evaluation mode which disables dropout.
-	    self.model.eval()
-	    if self.args.model == 'QRNN': self.model.reset()
-	    total_loss = 0
-	    ntokens = self.ntokens
-	    len_data_source = 0
+		self.model.eval()
+		if self.args.model == 'QRNN': self.model.reset()
+		total_loss = 0
+		ntokens = self.ntokens
+		len_data_source = 0
 
-	    # if we dump contexts, need a list to store them
-	    if not dump_vars is None: contexts = []
+		# if we dump contexts, need a list to store them
+		if not dump_vars is None: contexts = []
 
-	    # iterate over sequences of same length
-	    for seq_len, seq_data in data_source.items():
-	        for i in range(0, seq_data.size(0) - 1, seq_len):
+		# iterate over sequences of same length
+		for seq_len, seq_data in data_source.items():
+			for i in range(0, seq_data.size(0) - 1, seq_len):
 
-	            # new sequence -> reset hidden state
-	            hidden = self.model.init_hidden(batch_size)
+				# new sequence -> reset hidden state
+				hidden = self.model.init_hidden(batch_size)
 
-	            # get batch
-	            data, targets = get_batch(seq_data, i, args, seq_len=seq_len, evaluation=True)
+				# get batch
+				data, targets = get_batch(seq_data, i, args, seq_len=seq_len, evaluation=True)
 
-	            # evaluate
-	            output, new_hidden = self.model(data, hidden)
+				# evaluate
+				output, new_hidden = self.model(data, hidden)
 
-	            if self.args.loss == 'treelang_eucl':
-	                # need to augment output and targets with initial hidden state
-	                output = output.view(seq_len, self.batch_size, self.args.nhid)
-	                output = torch.cat((hidden[0], output.view(seq_len-1, eff_bsz, -1)), dim=0)
-	                targets = torch.cat((data[0].view(1), targets))
+				if self.args.loss == 'treelang_eucl':
+					# need to augment output and targets with initial hidden state
+					output = output.view(seq_len, self.batch_size, self.args.nhid)
+					output = torch.cat((hidden[0], output.view(seq_len-1, eff_bsz, -1)), dim=0)
+					targets = torch.cat((data[0].view(1), targets))
 
-	            hidden = new_hidden
-	            total_loss += len(data) * self.criterion(self.model, output, targets).data
-	            hidden = repackage_hidden(hidden)
+				hidden = new_hidden
+				total_loss += len(data) * self.criterion(self.model, output, targets).data
+				hidden = repackage_hidden(hidden)
 
-	            # collect context vectors
-	            if not dump_vars is None: contexts.append(output)
+				# collect context vectors
+				if not dump_vars is None: contexts.append(output)
 
-	        len_data_source += len(seq_data)
+			len_data_source += len(seq_data)
 
-	    # dump contexts
-	    if not dump_vars is None: dump_contexts(contexts, bsz=batch_size, **dump_vars)
+		# dump contexts
+		if not dump_vars is None: dump_contexts(contexts, bsz=batch_size, **dump_vars)
 
-	    # return loss
-	    return total_loss.item() / len_data_source
+		# return loss
+		return total_loss.item() / len_data_source
 
 
 	def _train(self):
@@ -259,73 +259,73 @@ class TinyLanguageModel():
 			self._train()
 
 			# this is if asgd is active
-		    if 't0' in self.optimizer.param_groups[0]:
-	            tmp = {}
-	            for prm in self.model.parameters():
-	                tmp[prm] = prm.data.clone()
-	                if 'ax' in self.optimizer.state[prm]:
-	                    prm.data = self.optimizer.state[prm]['ax'].clone()
+			if 't0' in self.optimizer.param_groups[0]:
+				tmp = {}
+				for prm in self.model.parameters():
+					tmp[prm] = prm.data.clone()
+					if 'ax' in self.optimizer.state[prm]:
+						prm.data = self.optimizer.state[prm]['ax'].clone()
 
-	            val_loss2 = self._evaluate(self.val_data, self.eval_batch_size)
-	            print('-' * 89)
-	            print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-	                'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
-	                    epoch, (time.time() - epoch_start_time), val_loss2, val_loss2, val_loss2 / math.log(2)))
-	            print('-' * 89)
+				val_loss2 = self._evaluate(self.val_data, self.eval_batch_size)
+				print('-' * 89)
+				print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+					'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
+						epoch, (time.time() - epoch_start_time), val_loss2, val_loss2, val_loss2 / math.log(2)))
+				print('-' * 89)
 
-	            if val_loss2 < stored_loss:
-	                self._model_save(self.args.save)
-	                print('Saving Averaged!')
-	                stored_loss = val_loss2
+				if val_loss2 < stored_loss:
+					self._model_save(self.args.save)
+					print('Saving Averaged!')
+					stored_loss = val_loss2
 
-	            for prm in self.model.parameters():
-	                prm.data = tmp[prm].clone()
+				for prm in self.model.parameters():
+					prm.data = tmp[prm].clone()
 
-	            self.val_loss.append(val_loss2)
+				self.val_loss.append(val_loss2)
 
-	        # otherwise
-	        else:
-	            val_loss = self._evaluate(self.val_data, self.eval_batch_size)
-	            print('-' * 89)
-	            print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-	                'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
-	              epoch, (time.time() - epoch_start_time), val_loss, val_loss, val_loss / math.log(2)))
-	            print('-' * 89)
+			# otherwise
+			else:
+				val_loss = self._evaluate(self.val_data, self.eval_batch_size)
+				print('-' * 89)
+				print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+					'valid ppl {:8.2f} | valid bpc {:8.3f}'.format(
+					epoch, (time.time() - epoch_start_time), val_loss, val_loss, val_loss / math.log(2)))
+				print('-' * 89)
 
-	            if val_loss < stored_loss:
-	                self._model_save(self.args.save)
-	                print('Saving model (new best validation)')
-	                stored_loss = val_loss
+				if val_loss < stored_loss:
+					self._model_save(self.args.save)
+					print('Saving model (new best validation)')
+					stored_loss = val_loss
 
-	            if self.args.optimizer == 'sgd' and 't0' not in self.optimizer.param_groups[0] and (len(best_val_loss)>self.args.nonmono and val_loss > min(best_val_loss[:-self.args.nonmono])):
-	                if self.asgd:
-	                	print('Switching to ASGD')
-	                	self.optimizer = torch.optim.ASGD(model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
+				if self.args.optimizer == 'sgd' and 't0' not in self.optimizer.param_groups[0] and (len(best_val_loss)>self.args.nonmono and val_loss > min(best_val_loss[:-self.args.nonmono])):
+					if self.asgd:
+						print('Switching to ASGD')
+						self.optimizer = torch.optim.ASGD(model.parameters(), lr=args.lr, t0=0, lambd=0., weight_decay=args.wdecay)
 
-	            if epoch in args.when:
-	                print('Saving model before learning rate decreased')
-	                model_save('{}.e{}'.format(args.save, epoch))
-	                print('Dividing learning rate by 10')
-	                self.optimizer.param_groups[0]['lr'] /= 10.
+				if epoch in args.when:
+					print('Saving model before learning rate decreased')
+					model_save('{}.e{}'.format(args.save, epoch))
+					print('Dividing learning rate by 10')
+					self.optimizer.param_groups[0]['lr'] /= 10.
 
-	            best_val_loss.append(val_loss)
+				best_val_loss.append(val_loss)
 
-	        # every dumpat iteration: store contexts to file for later plotting
-	        if self.args.dumpat > 0 and epoch % self.args.dumpat == 0:
-	            dump_vars = dict({'basepath': self.args.dumpto, 'epoch':epoch, 'hsz':self.args.nhid})
-	            self._evaluate(self.test_data, self.test_batch_size, dump_vars)
+			# every dumpat iteration: store contexts to file for later plotting
+			if self.args.dumpat > 0 and epoch % self.args.dumpat == 0:
+				dump_vars = dict({'basepath': self.args.dumpto, 'epoch':epoch, 'hsz':self.args.nhid})
+				self._evaluate(self.test_data, self.test_batch_size, dump_vars)
 
 
-	    # done with training!
+		# done with training!
 		# load best model and evaluate it on the test set
 		model_load(self.args.save)
 		test_loss = self._evaluate(self.test_data, self.test_batch_size)
 		print('=' * 89)
 		print('| End of training | test loss {:5.2f} | test ppl {:8.2f} | test bpc {:8.3f}'.format(
-	    	test_loss, math.exp(test_loss), test_loss / math.log(2)))
+			test_loss, math.exp(test_loss), test_loss / math.log(2)))
 		print('=' * 89)
 		print('| End of training | best loss {:5.2f} | best ppl {:8.2f} | best bpc {:8.3f}'.format(
-	    	stored_loss, math.exp(stored_loss), stored_loss / math.log(2)))
+			stored_loss, math.exp(stored_loss), stored_loss / math.log(2)))
 		print('=' * 89)
 
 		return stored_loss, test_loss

dd3156e16196e276514ce40d1cf3c5bf1c263c1d
Philipp Wirth <pwirth@student.ethz.ch>
2019-04-12 09:23:58 +0200

    forgot one

---
+++ run_ns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# load settings from config file
+#configfile=$1
+script="ns_main.py"
+path="data/treelang/tiny_poisson/"
+
+# run main
+echo "running script: "$script"..."
+python $script --data $path

92535e4c4a2b5865a6f129231a18ada483e9a2b5
Philipp Wirth <pwirth@student.ethz.ch>
2019-04-20 11:41:21 +0200

    first commit split ns

---
+++ treelang/sample.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import WeightedRandomSampler
+
+class NegativeSampler(nn.Module):
+
+	def __init__(self, nsamples, frequencies, exp=0.75):
+
+		self.nsamples = nsamples
+		self.frequencies = (frequencies / torch.sum(frequencies)).pow(exp)
+
+		super(NegativeSampler, self).__init__()
+
+	def forward(self, sequence, start, seq_len, cuda):
+
+		# we need nsamples*(seq_len+1)*seq_len/2 samples
+		nsamples = (self.nsamples * (seq_len + 1) * seq_len) // 2
+
+		# sample based on frequencies
+		wrs = WeightedRandomSampler(self.frequencies, nsamples)
+		samples = list(wrs)
+
+		# fill in
+		offset = 0
+		data = torch.zeros(seq_len, self.nsamples*seq_len, dtype=torch.long)
+		for i in range(seq_len):
+			step = (i+1)*nsamples
+
+			# fill in negative samples
+			index = torch.LongTensor([j for j in range(step)])
+			data[i].scatter(0, index, samples[offset:offset+step])
+
+			# fill in positive samples
+			index = torch.LongTensor([j for j in range(step, self.nsamples*seq_len)])
+			data[i].scatter(0, index, sequence[i])
+
+			offset += step
+
+		# return data
+		return torch.cat((sequence.view(-1,1), data), 1)
+
+
+		
